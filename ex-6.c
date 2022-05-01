9bec6064cde384fca6ee5095542cebf55bd9aebd
Fateh Singh <fatehsingh.sunny5@gmail.com>
2022-05-01 16:04:09 +0530

    6th program

---
+++ ex-6.c
@@ -0,0 +1,17 @@
+// Ex-6
+// w.a.p to print odd number
+#include <stdio.h>
+
+int main(){
+    int x;
+    printf("enter number ");
+    scanf("%d",&x);
+    if(x%2!=0)
+    {
+        printf("odd number");
+    }
+    else{
+        printf("even number");
+    }
+    return 0;
+}             
