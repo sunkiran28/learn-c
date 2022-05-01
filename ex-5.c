b5a15cbcf1a8afff9713ecc1644ab703496a806d
Fateh Singh <fatehsingh.sunny5@gmail.com>
2022-05-01 15:58:45 +0530

    5th program

---
+++ ex-5.c
@@ -0,0 +1,17 @@
+// Ex-5
+// w.a.p to print even number
+#include <stdio.h>
+
+int main(){
+    int x;
+    printf("enter number ");
+    scanf("%d",&x);
+    if(x%2==0)
+    {
+        printf("even number");
+    }
+    else{
+        printf("odd number");
+    }
+    return 0;
+}             
